929e86fbaba9f2fb8d7e4c681fb55186d654f3a8
doris <dychoi9298@gmail.com>
2020-08-07 19:41:54 +0900

    Updates

---
+++ precache-manifest.04f5bf8d14aff8acdabb6a133472370f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c552e899f6654c162bcc5400f5a97fe",
+    "url": "/doris_portfolio/index.html"
+  },
+  {
+    "revision": "65f3d0cc5ace6ab75d9d",
+    "url": "/doris_portfolio/static/js/2.091db4f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/doris_portfolio/static/js/2.091db4f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb83472a8b8963720236",
+    "url": "/doris_portfolio/static/js/main.407e1db2.chunk.js"
+  },
+  {
+    "revision": "f5297fa4b8774fdd2361",
+    "url": "/doris_portfolio/static/js/runtime-main.12c8cf94.js"
+  }
+]);
