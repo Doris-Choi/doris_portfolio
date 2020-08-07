bfd4ae91dd254f0e9ad5cab5f831f4bd0dda34f0
doris <dychoi9298@gmail.com>
2020-08-07 19:57:57 +0900

    please Update MY page... please...

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AppHeader from './components/AppHeader';
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <h1>Hello World!</h1>
       <span>I'm Doris from Korea</span>
       <p>업데이트가 되어야 하는데...</p>
+      <AppHeader />
     </div>
   );
 }
---
+++ src/components/AppHeader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AppHeader() {
+  return <div>이거 안나오기만 해봐...</div>;
+}
+
+export default AppHeader;
