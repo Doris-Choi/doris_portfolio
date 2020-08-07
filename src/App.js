d4819a5434ce3bd087e4a8ad489586f5c54df655
doris <dychoi9298@gmail.com>
2020-08-07 20:08:59 +0900

    is this page updated?

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import AppHeader from './components/AppHeader';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
       <span>I'm Doris from Korea</span>
-      <p>업데이트가 되어야 하는데...</p>
-      <AppHeader />
     </div>
   );
 }
