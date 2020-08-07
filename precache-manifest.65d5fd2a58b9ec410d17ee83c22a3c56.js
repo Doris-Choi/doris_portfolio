0f4488b50853c84d241558ac2fea81ae7bec83a4
doris <dychoi9298@gmail.com>
2020-08-07 19:58:18 +0900

    Updates

--- precache-manifest.65d5fd2a58b9ec410d17ee83c22a3c56.js
+++ precache-manifest.65d5fd2a58b9ec410d17ee83c22a3c56.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6b9d6d9b76a2ca91b8c9efbed09f1626",
+    "revision": "7369c35ba081af7a6ca2e8e00a86fb3e",
     "url": "/doris_portfolio/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/doris_portfolio/static/js/2.091db4f9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c79ddd380cb38bf63a12",
-    "url": "/doris_portfolio/static/js/main.93969af0.chunk.js"
+    "revision": "fa4bd82c011e9dcd826c",
+    "url": "/doris_portfolio/static/js/main.750d4a3a.chunk.js"
   },
   {
     "revision": "f5297fa4b8774fdd2361",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/doris_portfolio/precache-manifest.655e61136bb62e2be92acdbf24e66bab.js"
+  "/doris_portfolio/precache-manifest.65d5fd2a58b9ec410d17ee83c22a3c56.js"
 );
 
 self.addEventListener('message', (event) => {
