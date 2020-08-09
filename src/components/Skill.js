61b80b2f43576527d7c1a039e1b605263cfc0d0e
doris <dychoi9298@gmail.com>
2020-08-10 01:40:07 +0900

    skill icon에 hover 시 skill_name, skill_level 나타나도록 처리🙏

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../style/Skill.scss';
 
-function SkillIcon({ name }) {
+function SkillIcon({ name, level }) {
   const style = {
     backgroundColor: '#fff',
     backgroundImage: `url(${require('../style/logo/' + name + '.png')})`,
@@ -10,46 +10,55 @@ function SkillIcon({ name }) {
     backgroundRepeat: 'no-repeat',
   };
   return (
-    <div className={`SkillIcon ${name}`} style={style}>
-      {name}
+    <div className="SkillIcon" style={style}>
+      <div className="status--hover">
+        <div>{name}</div>
+        <div>{level}</div>
+      </div>
     </div>
   );
 }
 function Skill() {
+  const level = {
+    basic: 'Basic',
+    intermediate: 'Intermediate',
+    advanced: 'Advanced',
+  };
+
   return (
     <div className="Skill">
       <h2>SKILLs</h2>
       <div>
         <h3>LANGUAGE</h3>
-        <SkillIcon name="Python" />
-        <SkillIcon name="JavaScript" />
-        <SkillIcon name="C" />
+        <SkillIcon name="Python" level={level.intermediate} />
+        <SkillIcon name="JavaScript" level={level.intermediate} />
+        <SkillIcon name="C" level={level.basic} />
       </div>
       <div>
         <h3>WEB</h3>
-        <SkillIcon name="HTML" />
-        <SkillIcon name="CSS" />
-        <SkillIcon name="Sass" />
-        <SkillIcon name="React" />
+        <SkillIcon name="HTML" level={level.intermediate} />
+        <SkillIcon name="CSS" level={level.intermediate} />
+        <SkillIcon name="Sass" level={level.intermediate} />
+        <SkillIcon name="React" level={level.intermediate} />
       </div>
       <div>
         <h3>BACK?</h3>
-        <SkillIcon name="Nodejs" />
+        <SkillIcon name="Nodejs" level={level.basic} />
       </div>
       <div>
         <h3>DATABASE</h3>
-        <SkillIcon name="MySQL" />
+        <SkillIcon name="MySQL" level={level.intermediate} />
       </div>
       <div>
         <h3>ETC</h3>
-        <SkillIcon name="GitHub" />
-        <SkillIcon name="Linux" />
+        <SkillIcon name="GitHub" level={level.intermediate} />
+        <SkillIcon name="Linux" level={level.basic} />
       </div>
       <div>
         <h3>DATA ANALYSIS</h3>
-        <SkillIcon name="R" />
-        <SkillIcon name="GA" />
-        <SkillIcon name="Firebase" />
+        <SkillIcon name="R" level={level.intermediate} />
+        <SkillIcon name="GA" level={level.basic} />
+        <SkillIcon name="Firebase" level={level.basic} />
         {/* <SkillIcon name="ML" /> */}
         {/* <SkillIcon name="NN" /> */}
       </div>
